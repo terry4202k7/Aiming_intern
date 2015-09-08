3b3ddfff0b9e22206b6035636af0099dce9518e0
tomoki kitaguchi <aimosakaguest01@gmail.com>
2015-09-08 17:14:39 +0900

    ログインページのパース完了　　UI は適当　

---
+++ Aiming/Assets/SaveId.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class SaveId : MonoBehaviour {
+	
+	string strId, strRo;
+	public InputField inputFieldId,inputFieldRo ;
+	//public Text textId;
+
+	public void SaveTextId () {
+		strId = inputFieldId.text;
+		strRo = inputFieldRo.text;
+		Debug.Log (strId);
+		StartCoroutine(GetData());
+
+}
+
+	 IEnumerator GetData(){
+
+		Debug.Log("in get data");
+		string url = "192.168.33.11:3000/users/login";
+		WWWForm form = new WWWForm();
+		form.AddField("name", strId);
+		form.AddField("room_no", strRo);
+		WWW www = new WWW(url, form);
+		yield return www;
+		if (www.error == null) {
+			Debug.Log(www.text);
+			var textAsset =www;
+			var jsonText = textAsset.text;
+			
+			// 文字列を json に合わせて構成された辞書に変換
+			var json = Json.Deserialize (jsonText) as Dictionary<string, object>;
+			Debug.Log (json["user_id"]);
+			Debug.Log (json["play_id"]);
+			Debug.Log (json["state"]);
+			Debug.Log (json["role"]);
+		}
+	}
+
+	}
+
---
+++ Aiming/Assets/logIn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logIn : MonoBehaviour {
+	public void ClickTest () {
+		Debug.Log ("Clicked.");
+		}
+
+
+}
